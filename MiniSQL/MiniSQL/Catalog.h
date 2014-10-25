a0af64bdbe71780f1eff6ba37af54a1077c048fe
yhyhyh <893132051@qq.com>
2014-10-25 20:45:14 +0800

    Add attr_info
    Add attr_info in Catalog.h

--- MiniSQL/MiniSQL/Catalog.h
+++ MiniSQL/MiniSQL/Catalog.h
@@ -2,5 +2,10 @@
 #define _CATALOG_H
 #include <afx.h>
 #include <fstream>
+struct attr_info{
+	char name[20],index_name[20];
+	char data_type,attr_type;
+	int  data_len,num;
+};
 void Create_Database(CString DB_Name);
 #endif
