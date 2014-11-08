307b7da56abb67fba3c2fbe6655f131314bef35b
yhyhyh <893132051@qq.com>
2014-11-08 19:53:20 +0800

    Can select *

--- Record.cpp
+++ Record.cpp
@@ -1,36 +1,36 @@
 #include "Record.h"
-#include <string.h>
  
-void Close_Database(CString DB_Name,bool closetype);
-void Close_File(CString DB_Name,CString filename,int filetype,bool closetype);
-void Insert_Item(CString DB_Name,CString Table_Name,CString Attr,int & record_Num);
+void Close_Database(string DB_Name,bool closetype);
+void Close_File(string DB_Name,string filename,int filetype,bool closetype);
+void Insert_Item(string DB_Name,string Table_Name,string Attr,int & record_Num);
 void Print_Head(attr_info print[32],int count){
     int i;
-    for (i=0;i<count;i++)
-        printf("%s\t",print[i].name);
+    //for (i=0;i<count;i++)
+        //printf("%s\t",print[i].name);
 }
 
-void Print_To_Screen(CString record,attr_info print[32],int count);
+void Print_To_Screen(string record,attr_info print[32],int count);
 
-void Select_Without_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,attr_info print[32],int Count,char cond){
+void Select_Without_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,attr_info print[32],int Count,char cond){
     if (NULL==conds[0].left)
         Select_No_Where(DB_Name,Table_Name,print,count); 
     else
         Select_With_Where(DB_Name,Table_Name,conds,count,cond,print,Count);
 }
 
-void Select_With_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,attr_info print[32],int Count,char cond,int index);
-void Select_With_Equal_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,attr_info print[32],int Count,char cond,index_info Index);
-void Select_With_Greater_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
-void Select_With_Smaller_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
+void Select_With_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,attr_info print[32],int Count,char cond,int index);
+void Select_With_Equal_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,attr_info print[32],int Count,char cond,index_info Index);
+void Select_With_Greater_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
+void Select_With_Smaller_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
 
-bool Confirm_To_Where(CString record,condition_info conds[10],int count,char cond){
+bool Confirm_To_Where(string record,conditionInfo conds[10],int count,char cond){
      int i;
      if ('a'==cond){                // represent and
          for (i=0;i<count;i++)
          if (false==Confirm(record,conds[i]))
             return false;
          return true;
+	 }
      else{                        // represent or
          for (i=0;i<count;i++)
          if (true==Confirm(record,conds[i]))
@@ -39,12 +39,13 @@ bool Confirm_To_Where(CString record,condition_info conds[10],int count,char con
      }  
 }
 
-bool Confirm(CString record,condition_info condition){
+bool Confirm(string record,conditionInfo condition){
      int attr,i;
-     char *elem;
+     char *detail[10],*elem,*record_c;
      
+     strcpy(record_c,record.c_str());
      attr=condition.left->num;    
-     elem=strtok(record,esplit);
+     elem=strtok(record_c,esplit);
      i=0;
      while (elem!=NULL){
          detail[i]=elem;
@@ -52,59 +53,70 @@ bool Confirm(CString record,condition_info condition){
          elem=strtok(NULL,esplit);
      }
      switch (condition.symbol){
-         case -1: if (detail[attr]>=condition.right)
+         case -1: if (detail[attr][0]>=condition.right)
                      return false;
-         case 0:  if (detail[attr]!=condition.right)
+         case 0:  if (detail[attr][0]!=condition.right)
                      return false;
-         case 1:  if (detail[attr]<=condition.right)
+         case 1:  if (detail[attr][0]<=condition.right)
                      return false;
      }
      return true;
 }
         
-void Select_No_Where(CString DB_Name,CString Table_Name,attr_info print[32],int count){
+void Select_No_Where(string DB_Name,string Table_Name,attr_info print[32],int count){
     blockInfo *head,*ptr;
     fileInfo *file;
-    int need[10],i,j;
-    char *line,detail[10][20];
+    int need[10],i,j,bi,li,lnum;
+    char *line,*detail[10],*elem,*line_c[100];
     
-    for (i=0;i<Count;i++)
+    for (i=0;i<count;i++)
         need[i]=print[i].num;
     
-    file=get_file_info(DB_Name,Table_Name,0,bufferInfo);
-    head=file->firstBlock;
-    for (ptr=head;ptr!=file->lastBlock;ptr=ptr->next){
-        line=strtok(ptr->block,lsplit);
+    bufferInfo *run;
+	run= new bufferInfo;
+    head=readBlock(DB_Name,Table_Name,0,0,run);
+    for (bi=0;bi<1;bi++){
+        line=strtok(head->cBlock,lsplit);
+        li=0;
         while (line!=NULL){
-            elem=strtok(record,esplit);
+              line_c[li]=line;
+              li++;
+              line=strtok(NULL,lsplit);
+        }
+        lnum=li;
+        
+        for (li=0;li<lnum;li++){
+            elem=strtok(line_c[li],esplit);
             i=0;
             while (elem!=NULL){
                 detail[i]=elem;
                 i++;
                 elem=strtok(NULL,esplit);
             }
-            for (j=0;j<Count;j++)
-               printf("%s\t",detail[need[j]]);
-            line=strtok(NULL,lsplit);
-        } 
-    }
+            for (j=0;j<=count;j++)
+               printf("%s\t",detail[j]);
+        }
+    } 
 }
 
-void Select_With_Where(CString DB_Name,CString Table_Name,condition_info conds[10],int count,char cond,attr_info print[32],int Count){
+void Select_With_Where(string DB_Name,string Table_Name,conditionInfo conds[10],int count,char cond,attr_info print[32],int Count){
     blockInfo *head,*ptr;
     fileInfo *file;
     int need[10],i,j;
-    char *line,detail[10][20];
+    char *line,*detail[10],*elem,*line_c;
     
     for (i=0;i<Count;i++)
         need[i]=print[i].num;
     
-    file=get_file_info(DB_Name,Table_Name,0,bufferInfo);
+    bufferInfo *run;
+	run= new bufferInfo;
+    file=getfile(DB_Name,Table_Name,0,run);
     head=file->firstBlock;
     for (ptr=head;ptr!=file->lastBlock;ptr=ptr->next){
-        line=strtok(ptr->block,lsplit);
+        line=strtok(ptr->cBlock,lsplit);
         while (line!=NULL){
-            elem=strtok(record,esplit);
+            strcpy(line_c,line);
+            elem=strtok(line_c,esplit);
             i=0;
             while (elem!=NULL){
                 detail[i]=elem;
@@ -119,13 +131,28 @@ void Select_With_Where(CString DB_Name,CString Table_Name,condition_info conds[1
     }
 }
 
-void Delete_With_Where(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond);
-void Delete_Without_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond);
-void Delete_With_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond,int index);
-void Delete_With_Equal_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond,index_info Index);
-void Delete_With_Greater_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
-void Select_With_Smaller_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
-bool Find_Useful_Cond(condition_info conds[10],int count,int & index);
-void Insert_Index_All(CString DB_Name,CString Table_Name,CString Index_Name,int length,int offset,int type);
-bool Verify_Insertable(CString DB_Name,CString Table_Name,index_info nodes[32],int count,CString Attr);
-void Quit(CString DB_Name);
+void Delete_With_Where(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond);
+void Delete_Without_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond);
+void Delete_With_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond,int index);
+void Delete_With_Equal_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond,index_info Index);
+void Delete_With_Greater_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
+void Select_With_Smaller_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
+bool Find_Useful_Cond(conditionInfo conds[10],int count,int & index);
+void Insert_Index_All(string DB_Name,string Table_Name,string Index_Name,int length,int offset,int type);
+bool Verify_Insertable(string DB_Name,string Table_Name,index_info nodes[32],int count,string Attr);
+void Quit(string DB_Name);
+
+int main(){
+    attr_info print[32];
+    attr_info *f;
+    f=new attr_info;
+    f->num=0;
+    print[1]=*f;
+    //createDatabase("D_1");
+    //createTable("D_1", "Balance");
+    addAttr("D_1", "Balance", "a", 8, 0, 1);
+    addAttr("D_1", "Balance", "b", 0, 0, 0);
+    Select_No_Where("D_1","Balance",print,1);
+    printf("2333\n");
+    while(1);
+}
--- Record.h
+++ Record.h
@@ -5,92 +5,52 @@
 #define MAX_FILE_ACTIVE 	5		// limit the active files in the buffer
 #define MAX_BLOCK			40		// the max number of the blocks
 #define LinB                8
-#include "afx.h"
 #include <time.h>
 #include <fstream>
 #include <set>
+#include "Buffer.h"
+using namespace std;
 #include "Catalog.h"
 
-struct fileInfo;
-struct blockInfo;
-struct bufferInfo{
-public:
-	fileInfo *fileHandle;
-	blockInfo *blockHandle;
-	int fileCount;
-	int blockCount;
-	bufferInfo() :fileHandle(NULL), blockHandle(NULL),fileCount(0){};
+struct attr_info{
+	int num;
+	string name;
 };
 
-struct blockInfo  {
-	int blockNum;	// the block number of the block,
-	// which indicate it when it be newed
-	bool dirtyBit;     // 0 -> flase£¬ 1 -> indicate dirty, write back
-	blockInfo *next;  	// the pointer point to next block 
-	fileInfo *file;     	// the pointer point to the file, which the block belongs to
-//  int charNum;	   	// the number of chars in the block
-	char *cBlock;	   	// the array space for storing the records in the block in buffer
-	time_t iTime;		// it indicate the age of the block in use 
-	int lock;// prevent the block from replacing
-	blockInfo()
-	{
-		cBlock = new char[BLOCK_LEN];
-	}
-	~blockInfo()
-	{
-		delete[] cBlock;
-	}
-};
-
-struct fileInfo  {
-	int type;				// 0-> data file£¬ 1 -> index file
-	CString fileName;		// the name of the file
-	CString dataBase;       // dataBase it belongs to
-	int recordAmount;		// the number of record in the file
-//	int freeNum;			// the free block number which could be used for the file
-	int recordLength;		// the length of the record in the file
-	fileInfo *next;			// the pointer points to the next file
-	blockInfo *firstBlock;	// point to the first block within the file
-	blockInfo *lastBlock;
-	std::set<int> blockSet;
-	fileInfo(CString dataBase, CString fileName, int type) :
-		dataBase(dataBase), fileName(fileName), type(type), next(NULL), firstBlock(NULL), lastBlock(NULL)
-	{
-		blockSet.clear();
-	};
-};	
-
-struct condition_info{
+struct conditionInfo{
     int type,symbol;
     attr_info *left;
     int right;
 };
 
+struct index_info{
+};
+
 char *lsplit=" ",*esplit=",";
 
-void Close_Database(CString DB_Name,bool closetype);
-void Close_File(CString DB_Name,CString filename,int filetype,bool closetype);
-void Insert_Item(CString DB_Name,CString Table_Name,CString Attr,int & record_Num);
+void Close_Database(string DB_Name,bool closetype);
+void Close_File(string DB_Name,string filename,int filetype,bool closetype);
+void Insert_Item(string DB_Name,string Table_Name,string Attr,int & record_Num);
 void Print_Head(attr_info print[32],int count);
-void Print_To_Screen(CString record,attr_info print[32],int count);
-void Select_Without_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,attr_info print[32],int Count,char cond);
-void Select_With_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,attr_info print[32],int Count,char cond,int index);
-void Select_With_Equal_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,attr_info print[32],int Count,char cond,index_info Index);
-void Select_With_Greater_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
-void Select_With_Smaller_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
-bool Confirm_To_Where(CString record,condition_info conds[10],int count,char cond);
-bool Confirm(CString record,condition_info condition);
-void Select_No_Where(CString DB_Name,CString Table_Name,attr_info print[32],int count);
-void Select_With_Where(CString DB_Name,CString Table_Name,condition_info conds[10],int count,char cond,attr_info print[32],int Count);
-void Delete_With_Where(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond);
-void Delete_Without_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond);
-void Delete_With_Useful_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond,int index);
-void Delete_With_Equal_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond,index_info Index);
-void Delete_With_Greater_Cond(CString DB_Name,CString Table_Name,condition_info conds[10],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
-void Select_With_Smaller_Cond(CString DB_Name,CString Table_Name,condition_info conds[32],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
-bool Find_Useful_Cond(condition_info conds[10],int count,int & index);
-void Insert_Index_All(CString DB_Name,CString Table_Name,CString Index_Name,int length,int offset,int type);
-bool Verify_Insertable(CString DB_Name,CString Table_Name,index_info nodes[32],int count,CString Attr);
-void Quit(CString DB_Name);
+void Print_To_Screen(string record,attr_info print[32],int count);
+void Select_Without_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,attr_info print[32],int Count,char cond);
+void Select_With_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,attr_info print[32],int Count,char cond,int index);
+void Select_With_Equal_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,attr_info print[32],int Count,char cond,index_info Index);
+void Select_With_Greater_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
+void Select_With_Smaller_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,attr_info print[32],int Count,char cond,index_info Index,int type);
+bool Confirm_To_Where(string record,conditionInfo conds[10],int count,char cond);
+bool Confirm(string record,conditionInfo condition);
+void Select_No_Where(string DB_Name,string Table_Name,attr_info print[32],int count);
+void Select_With_Where(string DB_Name,string Table_Name,conditionInfo conds[10],int count,char cond,attr_info print[32],int Count);
+void Delete_With_Where(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond);
+void Delete_Without_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond);
+void Delete_With_Useful_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond,int index);
+void Delete_With_Equal_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond,index_info Index);
+void Delete_With_Greater_Cond(string DB_Name,string Table_Name,conditionInfo conds[10],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
+void Select_With_Smaller_Cond(string DB_Name,string Table_Name,conditionInfo conds[32],int count,index_info nodes[32],int num,char cond,index_info Index,int type);
+bool Find_Useful_Cond(conditionInfo conds[10],int count,int & index);
+void Insert_Index_All(string DB_Name,string Table_Name,string Index_Name,int length,int offset,int type);
+bool Verify_Insertable(string DB_Name,string Table_Name,index_info nodes[32],int count,string Attr);
+void Quit(string DB_Name);
 
 #endif
