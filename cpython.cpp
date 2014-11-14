6df738ffe05d1fb9f0a585498d4d3d7ab232b78e
Bran <544524199@qq.com>
2014-11-15 00:21:18 +0800

    Run Embeded python in C on Mac OS x

---
+++ cpython.cpp
@@ -0,0 +1,25 @@
+#include <python2.7/Python.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+ const char* pBuffer = NULL;
+ int iBufferSize = 0;
+ Py_Initialize();
+ PyRun_SimpleString("import sys\nsys.path.append('./')");
+ PyObject * pModule = NULL;
+ PyObject * pFunc   = NULL,*pValue=NULL;
+ //cout<<"ok"<<endl;
+ pModule = PyImport_ImportModule("Inter");
+ //cout<<"ok"<<endl;
+ pFunc   = PyObject_GetAttrString(pModule, "interpreter");
+ //cout<<"ok"<<endl;
+ pValue=PyEval_CallObject(pFunc, NULL);
+ PyArg_Parse(pValue, "s", &pBuffer);
+ //cout<<"ok"<<endl;
+ Py_Finalize();
+ cout<<pBuffer<<endl;
+ return 0;
+}
+
+
