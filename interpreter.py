7754ce5e8f355a651cd78fe47503810f0d05936e
Bran <544524199@qq.com>
2014-11-15 23:31:46 +0800

    fix bug

--- interpreter.py
+++ interpreter.py
@@ -6,7 +6,7 @@ def interpreter():
         if "from" not in s:
             print "No table are seleceted!"
         elif "where" in s:
-            m = re.match(r"select(.*)from(.*)where(.*)$", s)
+            m = re.match(r"select(.*)from(.*)where(.*);", s)
             if m:
                 s1=m.group(1).replace(" ","")
                 s2=m.group(2).replace(" ","")
@@ -16,25 +16,25 @@ def interpreter():
                 s3=s3.replace("or","|");
                 s="21"+s1+","+s2+","+s3
                 return s
-            else:
-                m = re.match(r"select(.*)from(.*)$", s)
-                s1=m.group(1).replace(" ","")
-                s2=m.group(2).replace(" ","")
-                s1=s1.replace(",",".");
-                s="20"+s1+","+s2
-                return s
+        else:
+            m = re.match(r"select(.*)from(.*);", s)
+            s1=m.group(1).replace(" ","")
+            s2=m.group(2).replace(" ","")
+            s1=s1.replace(",",".");
+            s="20"+s1+","+s2
+            return s
     
     sql=""
     sql=raw_input(">>")
     while True:
-        s=raw_input()
-        if '$' in s:    #$为结束字符
+        if ';' in sql:    #$为结束字符
             break
+        s=raw_input()
         sql+=" "
         sql+=s
-    print sql
     if "select" in sql:
         sql=select_clause(sql)
-    print sql
+    #sql=sql.replace("$","")
+    return sql
 
-interpreter()
+#interpreter()
